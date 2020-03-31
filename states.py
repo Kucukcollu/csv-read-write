17f799a8fb44c4a78c6c2d6a9fee07bf76213584
Ali Aydın KÜÇÜKÇÖLLÜ <kucukcollu@outlook.com>
2020-03-31 18:01:52 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,14 +1,14 @@
-import csv 													# İmport Comma Seperated Values Module
+import csv   # İmport Comma Seperated Values Module
 
-with open("states_us.csv", "r") as states: 					# Read the file
+with open("states_us.csv", "r") as states:  # Read the file
 	csv_read = csv.DictReader(states)
 
-	with open("new_states.csv", "w") as new_states:			# Create a file to write
+	with open("new_states.csv", "w") as new_states:	 # Create a file to write
 		fieldnames = ["State","Abbreviation"]
 
 		csv_write = csv.DictWriter(new_states, fieldnames=fieldnames, delimiter="\t") # Seperate the file with tab
 
 		csv_write.writeheader()
 
-		for line in csv_read:								# Write the readed lines row by row
+		for line in csv_read:	# Write the readed lines row by row
 			csv_write.writerow(line)
