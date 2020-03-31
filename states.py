8d5a6f2f566d6d86464a070ccce40e4d23f1e167
Kucukcollu <kucukcollu@outlook.com>
2020-03-31 17:55:35 +0300

    CSV file readed and rewrited.

---
+++ states.py
@@ -0,0 +1,14 @@
+import csv 													# İmport Comma Seperated Values Module
+
+with open("states_us.csv", "r") as states: 					# Read the file
+	csv_read = csv.DictReader(states)
+
+	with open("new_states.csv", "w") as new_states:			# Create a file to write
+		fieldnames = ["State","Abbreviation"]
+
+		csv_write = csv.DictWriter(new_states, fieldnames=fieldnames, delimiter="\t") # Seperate the file with tab
+
+		csv_write.writeheader()
+
+		for line in csv_read:								# Write the readed lines row by row
+			csv_write.writerow(line)
